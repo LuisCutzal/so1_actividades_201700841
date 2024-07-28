dbe04c65b1867b7c2e46c62908afc997ffa298a0
LuisCutzal <2842258270404@ingenieria.usac.edu.gt>
2024-07-27 19:08:54 -0600

    creando la carpeta y el archivo saludos.log

--- actividad2/actividad2.sh
+++ actividad2/actividad2.sh
@@ -1,26 +1,29 @@
 #!/bin/bash
 
 # URL del API de GitHub
-GITHUB_USER="LuisCutzal"
 URL="https://api.github.com/users/"
 #echo "$GITHUB_USER"
 
 #Fecha
-fecha=$(date)
-echo "$fecha"
+fecha=$(date +'%d-%m-%Y')
+#echo "$fecha"
+
+#directorio carpeta tmp
+directorio="/tmp/$fecha"
+mkdir -p "$directorio"
+
 # Archivo de log
-LOGFILE="actividad2.log"
+LOGFILE="$directorio/saludos.log"
 
 # Consultar los datos del usuario
 response=$(curl -s -H "User-Agent: Mozilla/5.0" "$URL""$GITHUB_USER")
 
 # Extraer los datos usando jq
-username=$(echo "$response" | jq -r '.login')
 user_id=$(echo "$response" | jq -r '.id')
 created_at=$(echo "$response" | jq -r '.created_at')
 
 # Crear el mensaje
-message="Hola ${username}. User ID: ${user_id}. Cuenta fue creada el: ${created_at}."
+message="Hola ${GITHUB_USER}. el id del usuario es: ${user_id} la cuenta fue creada el: ${created_at}."
 
 # Escribir el mensaje en el archivo de log
 echo "$message" > "$LOGFILE"
